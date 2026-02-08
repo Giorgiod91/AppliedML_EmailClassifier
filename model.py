6bc4a3dc6009f1ee27422f2293be8d704e08110c
Giorgiod91 <giorgio.dettmar@gmx.de>
2026-02-08 11:34:06 +0100

    added gradient decent

--- model.py
+++ model.py
@@ -18,12 +18,47 @@ print(df.to_string())
 # sidmoid activation function
 
 def sigmoid(z):
+    """
+    Docstring for sigmoid
+    
+    :param z: Description
+    """
     return 1.0 / (1.0 np.exp(-z))
 
 
 
 #gradient
-@
+
 def calc_gradient(theta, X,y):
+    """
+    Docstring for calc_gradient
+    
+    :param theta: 
+    :param X: Contains all the features and all intances
+    :param y: Vector of the results
+    """    
     m = y.size # Numer of instances
-    return (X.T @ (sigmoid(X @ theta)) / m 
+    # add is the matrix multiplicaiton
+    return (X.T @ (sigmoid(X @ theta) -y ) / m
+
+
+
+def gradient_decent(X,y, alpha=0.1, num_iter=100, tol=1e-7):
+ """
+    Docstring for calc_gradient
+    
+    :param theta: 
+    :param X: Contains all the features and all intances
+    :param y: Vector of the results
+    """ 
+
+    X_b = np.c_[np.ones((X.shape[0],1)),X]
+
+    theta = np.zeros(X_n.shape[1])
+
+    for i in range(num_iter):
+                grad = calculate_gradient(theta, X, y)
+                theta -= alpha * grad 
+            if np.linalg.norm(grad) <tol:
+                break
+    return theta
